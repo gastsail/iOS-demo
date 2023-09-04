a6ccf598932285511746a09b34f2847ee132f5b6
gastonsaillen <gsaillen95@gmail.com>
2023-09-04 17:07:37 -0300

    First commit

---
+++ LandmarkTutorial/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  LandmarkTutorial
+//
+//  Created by Gaston Saillen on 04/09/2023.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ LandmarkTutorial/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  LandmarkTutorial
+//
+//  Created by Gaston Saillen on 04/09/2023.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
