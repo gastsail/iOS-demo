4bf700acfc80946c9a4bc7327faf91a871260b04
gastonsaillen <gsaillen95@gmail.com>
2023-09-04 15:05:49 -0300

    Initial Commit

---
+++ LandmarkTutorial/LandmarkTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkTutorialApp.swift
+//  LandmarkTutorial
+//
+//  Created by Gaston Saillen on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
