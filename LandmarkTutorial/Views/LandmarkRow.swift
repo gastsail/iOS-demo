776f833b83d5659a6cf5d791efe0d8bf22549d5d
gastonsaillen <gsaillen95@gmail.com>
2023-09-13 16:10:43 -0300

    Added landmarks json and list row

---
+++ LandmarkTutorial/Views/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  LandmarkTutorial
+//
+//  Created by Gaston Saillen on 13/09/2023.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
---
+++ LandmarkTutorial/Views/LandmarksList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksList.swift
+//  LandmarkTutorial
+//
+//  Created by Gaston Saillen on 13/09/2023.
+//
+
+import SwiftUI
+
+struct LandmarksList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarksList()
+}
